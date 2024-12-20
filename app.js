ebf73de36432f4dab549bfecd8b3f721aa7fb0ed
dipexplorer <dipexplorerid23@gmail.com>
2024-12-21 02:47:15 +0530

    adding updates

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
 
 // for static files
-
 app.use(express.static(path.join(__dirname, "public")));
 
 // update product
